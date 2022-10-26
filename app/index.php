a1bb60ff3b4ff318b6d5b2a4f44876bddc70c992
RicardoRGC <gonzalezricardo.c90@gmail.com>
2022-10-26 12:28:07 -0300

    api

--- app/index.php
+++ app/index.php
@@ -33,15 +33,15 @@ $app->addErrorMiddleware(true, true, true);
 // Add parse body
 $app->addBodyParsingMiddleware();
 /*
-Hacer un login para nuestra aplicacion;
-para esto vamos a necesitar añadir una columna mas a la tabla usuarios que ya tenemos en nuestro localhost
-tipo_perfil
-[empleado, cliente, admin]
-vamos a utilizar un middleware para poder chequear los perfiles de usuario en los request necesarios
-'/login' ese no deberia tener un middleware de proteccion
-por POST usuario y clave
-es usuario franco con perfil admin ingresó en la aplicacion
-*/
+ Hacer un login para nuestra aplicacion;
+ para esto vamos a necesitar añadir una columna mas a la tabla usuarios que ya tenemos en nuestro localhost
+ tipo_perfil
+ [empleado, cliente, admin]
+ vamos a utilizar un middleware para poder chequear los perfiles de usuario en los request necesarios
+ '/login' ese no deberia tener un middleware de proteccion
+ por POST usuario y clave
+ es usuario franco con perfil admin ingresó en la aplicacion
+ */
 
 // Routes
 $app->group('/usuarios', function (RouteCollectorProxy $group) {
@@ -51,11 +51,11 @@ $app->group('/usuarios', function (RouteCollectorProxy $group) {
   $group->post('/login', \LoginControllers::class . ':Verificar');
   $group->put('[/]', \UsuarioController::class . ':ModificarUno');
   $group->delete('[/]', \UsuarioController::class . ':BorrarUno');
-});/*->add(new SalidaMiddleware())->add(new EntradaMiddlewares());*/
+}); /*->add(new SalidaMiddleware())->add(new EntradaMiddlewares());*/
 
-// $app->get('[/]', function (Request $request, Response $response) {
-//   $response->getBody()->write("Pagina RGraf");
-//   return $response;
-// });
+$app->get('[/]', function (Request $request, Response $response) {
+  $response->getBody()->write("Pagina RGraf");
+  return $response;
+});
 
 $app->run();
